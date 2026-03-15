5ca2d4b736a7f57e91fc553a9e7f0987e385897a
JaiThakur <thakurj7333@gmail.com>
2026-03-15 12:38:24 +0530

    Initial commit

---
+++ src/main/java/com/example/springbootwebtutuorial/SpringbootwebtutuorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebtutuorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebtutuorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwebtutuorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootwebtutuorial/SpringbootwebtutuorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebtutuorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebtutuorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
